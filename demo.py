6f572b081aa3b848305ce13e4c58db25e3f9e385
limengxun <limengxun45@outlook.com>
2022-09-03 21:11:04 +0800

    style: demo small update

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
+import random
 from typing import Iterable
 from mcservercontrol import Player, PlayerObserver, PlayerCommandObserver
 from mcservercontrol.timeUtils import TimeUtils
@@ -5,13 +6,14 @@ from mcservercontrol.timeUtils import TimeUtils
 
 class WelcomeObserver(PlayerObserver):
     """
-    Show welcome title after player logged in
+    Show random welcome title after player logged in
     """
     def onPlayerLogin(self, player: Player):
 
         def welcome():
+            choices = ["Welcome", "Hello", "Greetings", "你好", "欢迎", "哈咯"]
+            self.server.title(player, text = random.choice(choices), color= "random")
             self.server.title_setSubtitle(player, player.name)
-            self.server.title(player, text = "Hello", color= "random")
 
         # Show welcome title 3 seconds after user login
         self.server.schedule(welcome, 3)
@@ -66,7 +68,7 @@ class RemindAddictionCallback(PlayerObserver):
     def __call__(self):
         # The method will be added to daemon and run inside loop of another thread
         WARN_TOLERANCE =  3600
-        WARN_INTERVAL = 900
+        WARN_INTERVAL = 1200
 
         for player in self.players.values():
             # "time_last_warn" is not in player status by default
@@ -78,11 +80,10 @@ class RemindAddictionCallback(PlayerObserver):
 
                 if time_since_last_login > WARN_TOLERANCE and \
                         TimeUtils.nowStamp() - getattr(player.status, "time_last_warn") > WARN_INTERVAL:
-                    self.server.title(
-                        target=player, 
-                        ttype="actionbar",
-                        color="yellow",
-                        text="You have been playing for {} hours, is it too long?"\
+                    self.server.tellraw(
+                        target = player,
+                        color = "red",
+                        text = "You have been playing for {} hours, is it too long?"\
                             .format(round(time_since_last_login/3600, 1))
                     )
                     setattr(player.status, "time_last_warn", TimeUtils.nowStamp())
