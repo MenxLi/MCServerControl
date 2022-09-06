1a1058b6447910d2263fd24030eea63f8fe522fe
monsoon-pro <mengxunli@whu.edu.cn>
2022-09-06 13:04:13 +0800

    feat: dynamically add player status with status.setdefault

--- mcservercontrol/player.py
+++ mcservercontrol/player.py
@@ -1,6 +1,7 @@
 """
 Abstraction of the player
 """
+from typing import Any
 from .timeUtils import TimeUtils
 
 class PlayerStatus:
@@ -12,6 +13,15 @@ class PlayerStatus:
         for k, v in kwargs.items():
             setattr(self, k, v)
 
+    def setdefault(self, name: str, value: Any) -> Any:
+        if not hasattr(self, name):
+            setattr(self, name, value)
+        return getattr(self, name)
+
+    # For dynamically type checking
+    def __getattr__(self, name: str) -> Any: ...
+
+    def __setattr__(self, name: str, value: Any) -> None: ...
 
 class Player:
     def __init__(self, name: str, status_dict: dict = {}) -> None:
