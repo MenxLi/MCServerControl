7a307ce5ada2ee7f5dda7843a5ac1058313ff70a
limengxun <limengxun45@outlook.com>
2022-09-07 16:26:59 +0800

    feat: new addon - kill-items

--- mcservercontrol/addons/__init__.py
+++ mcservercontrol/addons/__init__.py
@@ -3,7 +3,7 @@ Some preset observers
 """
 
 import random
-from typing import Iterable
+from typing import Iterable, List
 from mcservercontrol import Player, PlayerObserver, PlayerCommandObserver
 from mcservercontrol.timeUtils import TimeUtils
 
@@ -37,7 +37,7 @@ class CommandSuicide(PlayerCommandObserver):
     """
     A command to kill the player itself
     """
-    def onTriggered(self, player: Player, args: Iterable):
+    def onTriggered(self, player: Player, args: List[str]):
         # Using server.cmd to send native minecraft server command
         self.server.cmd(f"/kill {player.name}")
         return super().onTriggered(player, args)
@@ -46,3 +46,40 @@ class CommandSuicide(PlayerCommandObserver):
         # it is necessary to implement the help entry
         return "Kill your self"
 
+
+class CommandKillItem(PlayerCommandObserver):
+    """
+    Command to kill all items
+    """
+
+    def onTriggered(self, player: Player, args: List[str]):
+
+        def _killItems():
+            self.server.cmd("/kill @e[type=item]")
+            self.server.say("Killed all items.")
+
+        if not args:
+            self.server.say("Will clear items in 1 minute.")
+            self.server.schedule(_killItems, 60)
+
+        elif args[0] == "now":
+            _killItems()
+
+        elif args[0].isnumeric():
+            delay = float(args[0])
+            self.server.say(f"Will clear items in {round(delay, 1)} seconds.")
+            self.server.schedule(_killItems, delay)
+
+        else:
+            self.server.tellraw(target=player,
+                                text = "Invalid arguments",
+                                color = "red")
+
+        return super().onTriggered(player, args)
+
+    def help(self) -> str:
+        to_show = [
+            "Kill all items, usage: {} [now/<delay>]".format(self.entry),
+            "By default delay=60s"
+        ]
+        return "\n".join(to_show)
