ff10f2c4cd4102fbd191fa556ff7cf828cf072f0
limengxun <limengxun45@outlook.com>
2022-09-08 12:20:11 +0800

    0.1.2

--- mcservercontrol/configReader.py
+++ mcservercontrol/configReader.py
@@ -36,6 +36,7 @@ _version_histories = [
     ("0.0.1", "init"),
     ("0.1.0", "Re-written with new abstractions: player, server, observer and listener"),
     ("0.1.1", "Player status using get/set, record player today's online time"), 
+    ("0.1.2", "Record scheduled task globally; showHelp method; warn invalid command; addons"), 
 ]
 
 VERSION, UPDATE_NOTE = _version_histories[-1]
